9d8d6d3c0cc23b69fdda90dd2a780a17faf3d723
ConorM16 <muganc@tcd.ie>
2017-10-16 22:34:21 +0100

    Near final program

--- src/BTTest.java
+++ src/BTTest.java
@@ -11,7 +11,7 @@ public class BTTest {
 	}
 	
 	@Test
-	public void testput() {
+	public void testTwoNodes() {
 		BinaryTree bt = new BinaryTree();
 		bt.putNode(10);
 		assertEquals("Number is present:",true, bt.contains(10));	
@@ -21,13 +21,108 @@ public class BTTest {
 		assertEquals("Number is present:",true, bt.contains(15));
 		
 		assertFalse("Number is not present", bt.contains(12));
-		
+		}
+	
+	@Test
+	public void testtenNodes(){
+		BinaryTree bt = new BinaryTree();
 		for(int i = 1; i < 10; i++)
 		{
 			bt.putNode(i);
 		}
 		
 		assertEquals("Number is present:",true, bt.contains(9));
-		}
+		assertEquals("Depth of 9 node:", 8, bt.depth(9));
+	}
+	
+	@Test
+	public void testDepth() {
+		BinaryTree bt = new BinaryTree();
+		bt.putNode(10);
+		assertEquals("Number is present:",true, bt.contains(10));	
+		assertEquals("Depth of 10 node:", 0, bt.depth(10));
+		
+		bt.putNode(15);
+		assertEquals("Number is present:",true, bt.contains(15));
+		assertEquals("Depth of 15 node:", 1, bt.depth(15));
+		
+		assertEquals("Depth of non-existent node:", 0, bt.depth(105));
+	}
+	
+	@Test
+	public void testAncestors(){
+		BinaryTree bt = new BinaryTree();
+		bt.putNode(10);
+		bt.putNode(15);
+		assertNull("Node has no ancestors", bt.ancestors(10));
+		
+		int [] expected = {10};
+		assertArrayEquals("Node 15 has ancestors:",expected, bt.ancestors(15));
+		
+		bt.putNode(1);
+		assertArrayEquals("Node 1 has ancestors:",expected, bt.ancestors(1));
+		
+		bt.putNode(12);
+		int [] expected2 = {10,15};
+		assertArrayEquals("Node 12 has ancestors:",expected2, bt.ancestors(12));		
+	}
+	
+	@Test 
+	public void testMultipleAncestors(){
+		BinaryTree bt = new BinaryTree();
+		
+		bt.putNode(10);
+		bt.putNode(15);
+		bt.putNode(3);
+		bt.putNode(1);
+		bt.putNode(5);
+		bt.putNode(12);
+		bt.putNode(18);
+		bt.putNode(14);
+		
+		int [] expected = {10,15,12};
+		assertArrayEquals("Node 14 has ancestors:",expected, bt.ancestors(14));
+		
+		int [] expected2 = {10,3};
+		assertArrayEquals("Node 5 has ancestors:",expected2, bt.ancestors(5));
+		
+		int [] expected3 = {10,15};
+		assertArrayEquals("Node 12 has ancestors:",expected3, bt.ancestors(12));
+	}
+	
+	@Test
+	public void testGCA(){
+		BinaryTree bt = new BinaryTree();
+		
+		bt.putNode(2);
+		bt.putNode(1);
+		bt.putNode(6);
+		bt.putNode(10);
+		bt.putNode(12);
+		bt.putNode(7);
+		bt.putNode(4);
+		
+		assertEquals("GCA of 6 and 1:",2, bt.gca(6, 1));
+		assertEquals("GCA of 12 and 7:",10, bt.gca(12, 7));
+		assertEquals("GCA of 4 and 7:",6, bt.gca(4, 7));
+	}
+	
+	@Test
+	public void testZeroInput(){
+		BinaryTree bt = new BinaryTree();
 
+		bt.putNode(0);
+		bt.depth(0);
+		bt.ancestors(0);
+		bt.gca(0,7);
+	}
+	
+	@Test 
+	public void testInvalidInput(){
+		BinaryTree bt = new BinaryTree();
+		
+		assertEquals("Character is not an integer.",0, bt.isInt("a"));
+		assertEquals("1 is an integer.",1, bt.isInt("1"));
+		//assertEquals("Character '1' is an integer.",1, bt.isInt(1));
+	}
 }
--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -19,7 +19,11 @@ public class BinaryTree {
 	    }
 	    
 	    public void putNode(int n){
-	    	root = put(root, n);
+	    	if(!zero(n,"put"))
+	    	{
+		    	root = put(root, n);
+		    	setParents(root, root);
+	    	}
 	    }
 	    
 	    private Node put(Node x, int n) {
@@ -43,6 +47,171 @@ public class BinaryTree {
 	    	else if (n > x.N) return containsNode(x.right,n);
 	    	else return true;
 	    }
-	//////
+	    
+	    private void setParents(Node x, Node prev)
+	    {
+	    	if(x.left != null) setParents(x.left, x);
+	    	if(x.right != null) setParents(x.right, x);
+	    	if(x != prev){
+	    		x.parent = prev;
+	    		//System.out.println("Parent of " + x.N + " = " + x.parent.N);
+	    	}
+	    }
+	    
+	    public int depth(int n)
+	    {
+	    	if(!zero(n,"depth"))
+	    	{
+		    	if((contains(n))&& (root.N != n)){
+		    		return nodeDepth(root, n, 0);	    		
+		    	}
+		    	else return 0;
+	    	}
+	    	return 0;
+	    }
+	    
+	    private int nodeDepth(Node x, int n, int count)
+	    {
+	    	if (x == null) return count;
+	        if      (n < x.N){
+	        	count++;
+	        	return nodeDepth(x.left, n, count);
+	        }
+	        else if (n > x.N){
+	        	count++;
+	        	return nodeDepth(x.right, n, count);
+	        }
+	        else{
+	        	return count;
+	        }
+	    }
+	    
+	    public int [] ancestors(int n){
+	    	if(!zero(n,"ancestor"))
+	    	{
+		    	if((contains(n))&& (root.N != n)){
+		    		int [] ancestors = new int[depth(n)];
+		    		findAncestors(root,n,ancestors,0);
+		    		return ancestors;
+		    	}
+		    	else return null;
+	    	}
+	    	else return null;
+	    }
+	    private void findAncestors(Node x, int n, int [] ancestors, int i)
+	    {
+	    	//if (x == null) return ancestors;
+	        if      (n < x.N){
+	        	ancestors[i] = x.N;
+	        	i++;
+	        	findAncestors(x.left, n, ancestors, i);
+	        }
+	        else if (n > x.N){
+	        	ancestors[i] = x.N;
+	        	i++;
+	        	findAncestors(x.right, n, ancestors, i);
+	        }
+	        else{
+	        	//return ancestors;
+	        	//System.out.println("Node found");
+	        }
+	        //return ancestors;
+	    }
+	    
+	    public int gca(int a, int b)
+	    {
+	    	if((!zero(a,"ancestor")) && !zero(b,"ancestor"))
+	    	{
+		    	int [] arr1 = ancestors(a);
+		    	int [] arr2 = ancestors(b);
+		    	if((arr1 == null) || (arr2 == null))
+		    	{
+		    		return 0;
+		    	}
+		    	else return gcaFind(ancestors(a),ancestors(b));
+		    }
+	    	else return 0;
+	    }
+	    
+	    /*
+	     *Greatest common ancestor 
+	     * 
+	     */
+	    private int gcaFind(int [] anc1, int [] anc2){
+	    	if(anc1.length >= anc2.length)
+	    	{
+	    		return compare(anc1,anc2);
+	    	}
+	    	else
+	    	{
+	    		return compare(anc2,anc1);
+	    	}
+	    }
+	    
+	    /*
+	     * anc1 > anc2
+	     */
+	    private int compare(int [] anc1, int [] anc2){
+	    	int i = 0;
+	    	int gca = -1;
+	    	while(i < anc2.length)
+	    	{
+	    		if(anc1[i] == anc2[i])
+	    		{
+	    			gca = anc1[i];
+	    		}
+	    		i++;
+	    	}
+	    	return gca;
+	    }
+	    
+	    private boolean zero(int n, String type){
+	    	if(n == 0)
+	    	{
+	    		if(type.equals("put"))
+	    		{
+	    			zeroPrint("put");
+	    		}
+	    		else if(type.equals("depth"))
+	    		{
+	    			zeroPrint("depth");
+	    		}
+	    		else zeroPrint("ancestor");
+	    		return true;
+	    	}
+	    	else return false;
+	    }
+	    
+	    private void zeroPrint(String type){
+	    	if(type.equals("put"))
+	    	{
+	    		System.out.println("Zero cannot be added to tree.");
+	    	}
+	    	else if(type.equals("depth"))
+	    	{
+	    		System.out.println("Zero is not present.");
+	    	}
+	    	else System.out.println("Zero has no ancestors.");
+	    }
+	    
+//	    public int isInt(String string)
+//	    {
+//	        try
+//	        {
+//	            return Integer.parseInt(string);
+//	        } catch (NumberFormatException ex)
+//	        {
+//	        	invalidInput();
+//	            return 0;
+//	        }
+//	    }
+//	    
+//	    private void invalidInput()
+//	    {
+//	    	System.out.println("Invalid input.");
+//	    }
+	    
+	   // private int parseInt()
+	    
 
 }
